9d0f94a0a8e689014689ed808c707738c9986f4a
email <myemail@mail.com>
2024-01-25 08:15:38 +0700

    Fitur terima untuk print bukti transaksi, kemudian permainan status di page terima.

---
+++ review-day24.2/vite-project7/src/App.jsx
@@ -0,0 +1,67 @@
+import {
+  HashRouter,
+  Routes,
+  Route,
+  useLocation,
+  useParams,
+} from "react-router-dom";
+import { useState } from "react";
+import { ContextApplication } from "./libs/config/contexts";
+import PageCommonOutlet from "./pages/commons/PageCommonOutlet";
+import PageBarangList from "./pages/barang/PageBarangList";
+import PageBarangCreate from "./pages/barang/PageBarangCreate";
+import PageBarangDetail from "./pages/barang/PageBarangDetail";
+import PageTerimaList from "./pages/terima/PageTerimaList";
+import PageTerimaPrint from "./pages/terima/PageTerimaPrint";
+
+// const MyComponent = () => {
+//   //ambil params dari usenavigate pakai ini
+//   // const location = useLocation();
+
+//   //ambil dari Link pakai ini
+//   // const params = useParams()
+
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+
+//       {/* ambil usenavigate menggunakan useLocation */}
+//       {/* {JSON.stringify(location.state)} */}
+
+//       {/* ambil Link menggunakan */}
+//       {/* {JSON.stringify(params)} */}
+//     </div>
+//   );
+// };
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  return (
+    <>
+      <ContextApplication.Provider
+        value={{ isAuthenticated, setIsAuthenticated }}
+      >
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<PageCommonOutlet />}>
+              <Route index={true} element={<PageBarangList />} />
+              {/* useNavigate */}
+              <Route path={"new"} element={<PageBarangCreate />} />
+              {/* Link */}
+              {/* <Route path={"new/:id1/:id2/:id3"} element={<PageBarangCreate />} /> */}
+              <Route path={"detail/:id"} element={<PageBarangDetail />} />
+            </Route>
+
+            <Route path="/terima" element={<PageCommonOutlet />}>
+            <Route index={true} element={<PageTerimaList />} />
+            <Route path={"print"} element={<PageTerimaPrint />} />
+            </Route>
+          </Routes>
+        </HashRouter>
+      </ContextApplication.Provider>
+    </>
+  );
+};
+
+export default App;
